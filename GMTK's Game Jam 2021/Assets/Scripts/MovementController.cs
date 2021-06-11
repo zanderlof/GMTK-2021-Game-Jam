f1710056794d8058051adc673d525830d009d37f
zanderlof <zanderlof@gmail.com>
2021-06-11 12:03:18 -0700

    Basic Movement
    Basic Movement controller for player
    
    Still needs gravity and using the mouse to look around

---
+++ GMTK's Game Jam 2021/Assets/Scripts/MovementController.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public bool canMove;
+
+    private bool moveForward;
+    private bool moveBack;
+    private bool moveLeft;
+    private bool moveRight;
+    private bool jump;
+
+    public enum LastMove
+    {
+        Forward = 0,
+        Right,
+        Back,
+        Left
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canMove)
+        {
+            Movement();
+        }
+    }
+
+    void Movement()
+    {
+        //Vector to hold direction, zeroed out every frame.
+        Vector3 direction = Vector3.zero;
+
+        /*control movement up and down*/
+        if (moveForward)
+        {
+            direction += Vector3.forward;
+            moveForward = false;
+            
+        }
+
+        if (moveBack)
+        {
+            direction += Vector3.back;
+            moveBack = false;
+        }
+
+        /*control movement left and right*/
+        if (moveRight)
+        {
+            direction += Vector3.right;
+            moveRight = false;
+        }
+
+        if (moveLeft)
+        {
+            direction += Vector3.left;
+            moveLeft = false;
+        }
+
+        if (jump)
+        {
+            direction += Vector3.up;
+            jump = false;
+        }
+
+        //Translate this transform in direction.
+        transform.Translate(direction * Time.deltaTime);
+    }
+
+    public void MoveForward()
+    {
+        moveForward = true;
+    }
+
+    public void MoveBack()
+    {
+        moveBack = true;
+    }
+
+    public void MoveLeft()
+    {
+        moveLeft = true;
+    }
+
+    public void MoveRight()
+    {
+        moveRight = true;
+    }
+
+    public void Jump()
+    {
+        jump = true;
+    }
+}

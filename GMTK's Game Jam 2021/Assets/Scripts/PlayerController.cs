77db150e5cafc5bda6336580073d977fc858201b
zanderlof <zanderlof@gmail.com>
2021-06-12 15:29:09 -0700

    Switching Guns
    Able to switch guns to all 5 different types

--- GMTK's Game Jam 2021/Assets/Scripts/PlayerController.cs
+++ GMTK's Game Jam 2021/Assets/Scripts/PlayerController.cs
@@ -23,6 +23,7 @@ public class PlayerController : MonoBehaviour
     bool isMoving;
     bool movementChange;
     bool sizeChange;
+    int activeGun;
 
     // Start is called before the first frame update
     void Start()
@@ -44,6 +45,8 @@ public class PlayerController : MonoBehaviour
         Movement();
         // Aim();
         //Replace in favour of controlling cam directly
+
+        SwitchWeapon();
     }
 
     public void Movement()
@@ -115,4 +118,38 @@ public class PlayerController : MonoBehaviour
     // {
     //     transform.eulerAngles += lookSpeed * new Vector3(0, Input.GetAxis("Mouse X"), 0);
     // }
+
+    public void SwitchWeapon()
+    {
+        KeyCode gun = 0;
+        
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            gun = KeyCode.Alpha1;
+        }
+        else if (Input.GetKey(KeyCode.Alpha2))
+        {
+            gun = KeyCode.Alpha2;
+        }
+        else if (Input.GetKey(KeyCode.Alpha3))
+        {
+            gun = KeyCode.Alpha3;
+        }
+        else if (Input.GetKey(KeyCode.Alpha4))
+        {
+            gun = KeyCode.Alpha4;
+        }
+        else if (Input.GetKey(KeyCode.Alpha5))
+        {
+            gun = KeyCode.Alpha5;
+        }
+
+        if (gun != 0)
+        {
+            transform.GetChild(0).GetChild(activeGun).gameObject.SetActive(false);
+            activeGun = (int)gun - 49;
+            transform.GetChild(0).GetChild(activeGun).gameObject.SetActive(true);
+        }
+        
+    }
 }
